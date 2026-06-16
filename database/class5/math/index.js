9811e5b0e1f363f9be1186e38ea36fea5b551956
bijush <38435589+Bijush@users.noreply.github.com>
2026-06-16 17:43:58 +0530

    Add files via upload

--- database/class5/math/index.js
+++ database/class5/math/index.js
@@ -1,1 +1,31 @@
+import {
+angleLesson
+}
+from "./lesson2.js";
 
+import {
+LESSON1
+}
+from "./lesson1.js";
+
+import {
+LESSON3
+}
+from "./lesson3.js";
+
+import {
+LESSON4
+}
+from "./lesson4.js";
+
+export const mathLessons = {
+
+"পাঠ 2: কোণ":
+angleLesson,
+"পাঠ 1: সংখ্যা ও প্রক্রিয়া"
+:LESSON1,
+"পাঠ 3: কালি  ও পরিসীমা"
+:LESSON3,
+"পাঠ 4: গুণিতক ও  উৎপাদক"
+:LESSON4,
+};
