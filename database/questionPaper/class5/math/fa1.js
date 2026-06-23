713ffb0228e92f2dd222e75b546c97a398291c71
bijush <38435589+Bijush@users.noreply.github.com>
2026-06-23 09:56:36 +0530

    Add files via upload

---
+++ database/questionPaper/class5/math/fa1.js
@@ -0,0 +1,248 @@
+export const FA1 = {
+
+"পরীক্ষার_নাম":"প্রথম গঠনমূলক মূল্যায়ন – ২০২৬",
+"শ্রেণি":"V",
+"বিষয়":"গণিত",
+"পূর্ণমান":"৫০",
+"সময়":"২ ঘণ্টা",
+
+"১":{
+title:"সংক্ষিপ্তরূপে লিখ",
+marks:"১×৫=৫",
+questions:[
+"(i) 700 + 50 + 8 =",
+"(ii) 4000 + 300 + 20 + 5 =",
+"(iii) 9000 + 600 + 8 =",
+"(iv) 20000 + 5000 + 90 + 4 =",
+"(v) 60000 + 400 + 30 + 2 ="
+]
+},
+"২":{
+title:"অঙ্কে লিখ",
+marks:"১×৫=৫",
+questions:[
+
+"(i) নয় হাজার তিনশো সাত =",
+
+"(ii) পনেরো হাজার ছয় =",
+
+"(iii) আটাশ হাজার সাতশো দশ =",
+
+"(iv) চল্লিশ হাজার নয় =",
+
+"(v) বাহাত্তর হাজার পাঁচশো আট ="
+
+]
+},
+
+"৩":{
+title:"অক্ষরে লিখ",
+marks:"১×৫=৫",
+questions:[
+"(i) 18,205",
+"(ii) 29,010",
+"(iii) 61,507",
+"(iv) 70,091",
+"(v) 92,990"
+]
+},
+
+"৪":{
+title:"ছোট থেকে বড় ক্রমানুসারে সাজাও",
+marks:"১×৪=৪",
+questions:[
+{
+  text:"(i) 723, 960, 690, 732",
+answerBox:true,
+  answerBoxWidth:"150px"
+},
+{text:"(ii) 2823, 2832, 3282, 3228",
+  answerBoxWidth:"150px"
+},
+
+{
+  text:"(iii) 69002, 96200, 96002, 69200",answerBoxWidth:"150px"},
+  
+{
+  text:"(iv) 68625, 68256, 68652, 68265",
+answerBoxWidth:"150px"}
+]
+},
+
+"৫":{
+title:">, < অথবা = বসাও",
+marks:"১×৫=৫",
+questions:[
+"(i) 38640 ___ 48694 =",
+"(ii) 87878 ___ 87877 = ",
+"(iii) 60060 ___ 60600 = ",
+"(iv) 32098 ___ 32098 = ",
+"(v) 10010 ___ 11010 = "
+]
+},
+
+"৬":{
+title:"যোগ কর",
+marks:"২×৪=৮",
+questions:[
+
+{
+serial:"(i)",
+type:"verticalAddition",
+a:126,
+b:325
+},
+
+{
+serial:"(ii)",
+type:"verticalAddition",
+a:806,
+b:98
+},
+
+{
+serial:"(iii)",
+type:"verticalAddition",
+a:1234,
+b:891
+},
+
+{
+serial:"(iv)",
+type:"verticalAddition",
+a:18625,
+b:7928
+}
+
+]
+},
+
+"৭":{
+title:"বিয়োগ কর",
+marks:"২×৪=৮",
+questions:[
+
+{
+serial:"(i)",
+type:"verticalSubtraction",
+a:728,
+b:389
+},
+
+{
+serial:"(ii)",
+type:"verticalSubtraction",
+a:4316,
+b:729
+},
+
+{
+serial:"(iii)",
+type:"verticalSubtraction",
+a:8173,
+b:2954
+},
+
+{
+serial:"(iv)",
+type:"verticalSubtraction",
+a:26272,
+b:15729
+}
+
+]
+},
+
+"৮":{
+title:"গুণ কর",
+marks:"২×৪=৮",
+questions:[
+
+{
+serial:"(i)",
+type:"multiplication",
+a:6543,
+b:3
+},
+
+{
+serial:"(ii)",
+type:"multiplication",
+a:8469,
+b:5
+},
+
+{
+serial:"(iii)",
+type:"multiplication",
+a:8507,
+b:14
+},
+
+{
+serial:"(iv)",
+type:"multiplication",
+a:5621,
+b:84
+}
+
+]
+},
+
+"৯":{
+title:"সমস্যাগুলির সমাধান কর",
+marks:"৩×২=৬",
+questions:[
+
+"বিনোদের বাবা মাছ বিক্রি করে ২৫,৫০০ টাকা এবং হাঁস বিক্রি করে ২,৫০০ টাকা পেলেন। মোট কত টাকা পেলেন?",
+
+"রমার মায়ের মাসিক উপার্জন ১৫,০০০ টাকা। এর মধ্যে প্রতি মাসে ৫,০০০ টাকা পোস্ট অফিসে জমা রাখেন। এক বছরে মোট কত টাকা জমা হবে?"
+
+]
+},
+
+"১০":{
+title:"পাশ ও কোণের সংখ্যা লিখ",
+marks:"১×৮=৮",
+questions:[
+
+{
+type:"cube",
+sideLabel:"পাশ =",
+cornerLabel:"কোণ ="
+},
+
+{
+type:"cylinder",
+sideLabel:"পাশ =",
+cornerLabel:"কোণ ="
+},
+
+{
+type:"sphere",
+sideLabel:"পাশ =",
+cornerLabel:"কোণ ="
+},
+
+{
+type:"rectangle",
+sideLabel:"পাশ =",
+cornerLabel:"কোণ ="
+}
+
+]
+},
+
+"১১":{
+title:"পরিসীমা নির্ণয় কর",
+marks:"১×২=২",
+questions:[
+
+"দৈর্ঘ্য = ২০ সেমি, প্রস্থ = ১০ সেমি",
+
+"দৈর্ঘ্য = ২৪ সেমি, প্রস্থ = ৬ সেমি"
+
+]
+}
+
+};
